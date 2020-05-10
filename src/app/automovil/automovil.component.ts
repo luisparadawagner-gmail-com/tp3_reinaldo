8b191dbcdb73cdd776a5cb93e5345dca6114998e
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-05-10 19:53:05 -0300

    Agregado formulario de automovil

--- src/app/automovil/automovil.component.ts
+++ src/app/automovil/automovil.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl,FormGroup,FormBuilder,Validator,Validators,FormArray } from '@angular/forms';
 @Component({
   selector: 'app-automovil',
   templateUrl: './automovil.component.html',
   styleUrls: ['./automovil.component.css']
 })
-export class AutomovilComponent implements OnInit {
+export class AutomovilComponent {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  automovilForm = this.fb.group({
+    marca: ['Ford',Validators.required],
+    año: [''],
+    datosTecnicos: this.fb.group({
+      enMarcha: [''],
+      ultimoServicio: ['']
+    }),
+    mecanicosACargo: this.fb.array([ this.fb.control('')])
+    
+  });
+  
+  get mecanicosACargo() {
+
+    return this.automovilForm.get('mecanicosACargo') as FormArray;
   }
+
+  agregarMecanico(){
+    this.mecanicosACargo.push(this.fb.control(''));
+  }
+
+  submit() {
+    this.automovilForm.setValue({
+      marca: 'Volskwagen',
+      año: 2015,
+      datosTecnicos:{
+        enMarcha: true,
+        ultimoServicio: new Date()
+      },
+      mecanicosACargo: ['']
+    })
+
+    this.automovilForm.patchValue({año:2016});
+  }
+
+  
 
 }
